767c0e5e7fafdfe7e72d71012d5b8a2c9665215b
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2022-02-09 13:10:54 -0300

    cloneNetflix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect} from 'react';
+import RequestFetchUrl from './RequestFetchUrl';
 
 function App() {
+  
+  useEffect(() => {
+    const loadAll = async () => {
+      //pegando a lista de filmes
+      let list = await RequestFetchUrl.getHomeList()
+      console.log(list)
+    }
+    loadAll ()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello</h1>
     </div>
   );
 }
---
+++ src/RequestFetchUrl.js
@@ -0,0 +1,66 @@
+const API_KEY = '279416748d4a93333d67fbc025e236d4'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+/*
+ originais 
+ recomendados trending
+ em alta top rated
+ ação
+ comédia
+ terror
+ romance
+ decumentários
+*/
+
+const basicFetch = async (endPoint) => {
+    const req = await fetch(`${API_BASE}${endPoint}`)
+    const json = await req.json()
+    return json
+}
+
+export default {
+    getHomeList: async () =>{
+        return [
+            {
+                slug:'originals',
+                title: 'Originais netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug:'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?api_key=${API_KEY}`)
+            },
+            {
+                slug:'toprated',
+                title: 'Filmes em alta',
+                items: await basicFetch(`/movie/top_rated?api_key=${API_KEY}`)
+            },
+            {
+                slug:'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&api_key=${API_KEY}`)
+            },
+            {
+                slug:'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&api_key=${API_KEY}`)
+            },
+            {
+                slug:'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&api_key=${API_KEY}`)
+            },
+            {
+                slug:'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&api_key=${API_KEY}`)
+            },
+            {
+                slug:'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&api_key=${API_KEY}`)
+            },
+        ]
+    }
+}

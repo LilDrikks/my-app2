bcd111136af938ec7823e2591b146bbb2ec2159c
LilDrikks <87108295+LilDrikks@users.noreply.github.com>
2022-02-13 15:10:53 -0300

    Up3 Banner gradient e outros components

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,7 @@
 import styles from './Banner.module.scss'
+import {CgPlayButton, CgInfo} from 'react-icons/cg'
 const Banner = ({item}) => {
+    console.log(item)
     return ( 
         <section className={styles.banner} style={{
             backgroundSize:'cover',
@@ -8,7 +10,17 @@ const Banner = ({item}) => {
         }}>
             <div className={styles.gradientTop}>
                 <div className={styles.gradientRight}>
-                    <p>{item.name}</p>
+                    <p className={styles.title}>{item.name}</p>
+                    <p className={styles.vote}>Votos {item.vote_average}</p>
+                    <p className={styles.overview}>{item.overview}</p>
+                    <div className={styles.buttons}>
+                        <button>
+                            <CgPlayButton className={styles.cgPlayIcon}/> Assistir
+                        </button>
+                        <button className={styles.btnCgInfo}>
+                          <CgInfo className={styles.cgInfoIcon}/> Mais informações
+                        </button>
+                    </div>
                 </div>
             </div>
         </section>
